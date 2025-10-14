13ce128232d9fb650866a3caa9a4f7958561a104
csanlag861 <csanlag861@g.educaand.es>
2025-10-15 01:58:32 +0200

    Creacion de Repositorio & Creacion de página Inicio

---
+++ react/lovestar-ecommerce/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="contador">
+        <p>05D 13H 26M 27S</p>
+      </div>
+      <div className="text-lovestar">
+        <h1>Lovestar</h1>
+        <p>ENTRAR</p>
+      </div>
+      <div className="footer">
+        <p>'A LOVESTORY'</p>
+        <p>ÚNETE AL CLUB</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
