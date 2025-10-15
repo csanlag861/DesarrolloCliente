a7c144ea2e280d3c7055e03aa789a682e83d7050
csanlag861 <csanlag861@g.educaand.es>
2025-10-15 21:28:48 +0200

    Creacion de componente Intro + Contador y arreglos en el css

--- react/lovestar-ecommerce/src/App.jsx
+++ react/lovestar-ecommerce/src/App.jsx
@@ -1,21 +1,10 @@
 import "./App.css";
+import Intro from "./components/Intro";
 
 function App() {
-  return (
-    <div className="container">
-      <div className="contador">
-        <p>05D 13H 26M 27S</p>
-      </div>
-      <div className="text-lovestar">
-        <h1>Lovestar</h1>
-        <p>ENTRAR</p>
-      </div>
-      <div className="footer">
-        <p>'A LOVESTORY'</p>
-        <p>ÚNETE AL CLUB</p>
-      </div>
-    </div>
-  );
+  return <div className="container">
+    <Intro />
+  </div>;
 }
 
 export default App;
---
+++ react/lovestar-ecommerce/src/components/Intro.jsx
@@ -0,0 +1,20 @@
+import Contador from "./Contador";
+const Intro = () => {
+  return (
+    <>
+      <div className="contador">
+        <Contador />
+      </div>
+      <div className="text-lovestar">
+        <h1>Lovestar</h1>
+        <p>ENTRAR</p>
+      </div>
+      <div className="footer">
+        <p>'A LOVESTORY'</p>
+        <p>ÚNETE AL CLUB</p>
+      </div>
+    </>
+  );
+};
+
+export default Intro;
