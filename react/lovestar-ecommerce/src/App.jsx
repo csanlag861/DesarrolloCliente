1500579f0d730fbe1894178e35acc9b14bc6ad9c
csanlag861 <csanlag861@g.educaand.es>
2025-10-17 16:11:13 +0200

    Creacion de proyecto ReactRouterDOM

--- react/lovestar-ecommerce/src/App.jsx
+++ react/lovestar-ecommerce/src/App.jsx
@@ -1,11 +1,11 @@
-import "./App.css";
-import Intro from "./components/Intro";
+/* import "./App.css";
+ */import Intro from "./components/Intro";
 import Home from "./components/Home";
 
 function App() {
   return <div className="container">
-    <Intro />
-    {/* <Home /> */}
+{/*     <Intro />
+ */}    <Home />
   </div>;
 }
 
