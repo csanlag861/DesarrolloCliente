a14beb3189cbdcb831fff3325d9ca2a28cedf6f0
csanlag861 <csanlag861@g.educaand.es>
2025-10-31 17:28:04 +0100

    Crear base de datos si no está creada

--- react/lovestar-ecommerce/src/utils/firebase.js
+++ react/lovestar-ecommerce/src/utils/firebase.js
@@ -57,3 +57,31 @@ export const createAuthUserWithEmailAndPassword = async (email, password) => {
 
   return await createUserWithEmailAndPassword(auth, email, password);
 };
+
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
+  if (!userAuth) return;
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { email, displayName } = userAuth;
+    const createdAt = new Date();
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInformation,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+  return userDocRef;
+};
+
+export const signOutUser = async () => await signOut(auth);
