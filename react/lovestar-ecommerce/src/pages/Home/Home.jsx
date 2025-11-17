c84270a40012a72554caafee7428df21a5d95279
csanlag861 <csanlag861@g.educaand.es>
2025-11-17 20:08:00 +0100

    Dialog

--- react/lovestar-ecommerce/src/pages/Home/Home.jsx
+++ react/lovestar-ecommerce/src/pages/Home/Home.jsx
@@ -1,13 +1,25 @@
 import Hero from "../../components/Home/Hero/Hero";
+import Dialog from "../../components/Dialog/Dialog";
 import Inpsiracion from "../../components/Home/Inspiracion/Inspiracion";
+import { useEffect, useState } from "react";
 
 
 function Home() {
+  const [showDialog, setShowDialog] = useState(false);
+
+  useEffect(() => {
+    const time = setTimeout(() => {
+      setShowDialog(true);
+    }, 1000);
+
+    return () => clearTimeout(time);
+  }, [])
 
   return (
     <>
-        <Hero />
-        <Inpsiracion />
+      <Hero />
+      <Inpsiracion />
+      {showDialog && <Dialog/>}
     </>
   );
 }
