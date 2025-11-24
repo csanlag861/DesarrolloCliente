99bfaaec7b2c57c74c44b625c693b5c028a3dd2e
csanlag861 <csanlag861@g.educaand.es>
2025-11-24 18:05:15 +0100

    fix(detalles): Arreglar los imports de los detalles

--- react/lovestar-ecommerce/src/components/Cards/Card-Shop/Card.jsx
+++ react/lovestar-ecommerce/src/components/Cards/Card-Shop/Card.jsx
@@ -2,25 +2,35 @@ import styleCard from "./card.module.css";
 
 import { useState } from "react";
 
+import { useNavigate } from "react-router-dom";
+
 const Card = ({ card }) => {
   const [hover, setHover] = useState(false);
+  const navigate = useNavigate();
 
   const styleColor = {
     width: "10px",
     height: "10px",
     borderRadius: "1000px",
-    gap: "4px"
+    gap: "4px",
+  };
+
+  const handleDetail = (id) => {
+    navigate(`/Tienda/${id}`);
   };
   return (
-    <div className={styleCard.card}>
+    <div className={styleCard.card} onClick={() => handleDetail(card.id)}>
       <div className={styleCard.imagen}>
-        
-        <img 
-         src={hover ? card.url_r : card.url}
-         onMouseEnter={() => {setHover(true)}}
-         onMouseLeave={() => {setHover(false)}}
-         alt={card.alt} />
-
+        <img
+          src={hover ? card.url_r : card.url}
+          onMouseEnter={() => {
+            setHover(true);
+          }}
+          onMouseLeave={() => {
+            setHover(false);
+          }}
+          alt={card.alt}
+        />
       </div>
       <div className={styleCard.text}>
         <h3>{card.nombre}</h3>
@@ -28,7 +38,6 @@ const Card = ({ card }) => {
         <p className={styleCard.talla}>{card.talla}</p>
         <div className={styleCard.colores}>
           {card.colores.map((color, id) => (
-
             <div
               key={id}
               style={{ ...styleColor, backgroundColor: color }}
