ea6ee9d4231eb9252c5ad72039cfd564ed1a4706
csanlag861 <csanlag861@g.educaand.es>
2025-11-27 21:28:47 +0100

    feat(card-carrito): Card del carrito

--- react/lovestar-ecommerce/src/components/Cards/Card-Carrito/Card.jsx
+++ react/lovestar-ecommerce/src/components/Cards/Card-Carrito/Card.jsx
@@ -2,16 +2,28 @@ import stylesCard from "./card.module.css";
 
 import { Icon } from "@iconify/react";
 
-const Card = ({ id, nombre, talla, cantidad, precio, url, alt}) => {
+const Card = ({ id, nombre, talla, cantidad, precio, url, alt }) => {
 
     return (
         <div className={stylesCard.card}>
-            <img src={url} alt={alt} />
-            <h4>ID{id}</h4>
-            <h4>Nombre {nombre}</h4>
-            <h4>Talla {talla}</h4>
-            <h4>Precio {precio}</h4>
-            <h4>Cantidad{cantidad}</h4>
+            <div className={stylesCard.imagen}>
+                <img src={url} alt={alt} />
+            </div>
+            <div className={stylesCard.info}>
+                <div className={stylesCard.detalles}>
+                    <h2>{nombre}</h2>
+                    {talla && (<h4>Talla: {talla}</h4>)}
+                    <h4>Cantidad: {cantidad}</h4>
+                </div>
+                <div className={stylesCard.extras}>
+                    <button className={stylesCard.cerrar}>
+                        <Icon icon="material-symbols:close" />
+                    </button>
+                    <h2>€{precio}</h2>
+                </div>
+{/*                 
+                <h4>Cantidad{cantidad}</h4> */}
+            </div>
         </div>
     );
 };
--- react/lovestar-ecommerce/src/components/Carrito/Carrito.jsx
+++ react/lovestar-ecommerce/src/components/Carrito/Carrito.jsx
@@ -10,7 +10,7 @@ import Card from "../Cards/Card-Carrito/Card";
 
 const Carrito = ({ closeCarrito }) => {
   const [cerrar, setCerrar] = useState(false);
-  const { carrito } = useContext(CartContext);
+  const { carrito, vaciarCarrito } = useContext(CartContext);
 
   const handleClose = () => {
     setCerrar(true);
@@ -34,11 +34,14 @@ const Carrito = ({ closeCarrito }) => {
       <hr />
       <img src="/img/favicon.svg" alt="" />
       {carrito ? (
-        <div className={stylesCarrito.cards} >
-          {carrito.map((prod, index) => (
-            <Card key={index} id={prod.id} nombre={prod.nombre} talla={prod?.talla} precio={prod.precio_descuento ? prod.precio_descuento : prod.precio} cantidad={prod.cantidad} url={prod.url} alt={prod.alt} />
-          ))}
-        </div>
+        <>
+          <div className={stylesCarrito.cards} >
+            {carrito.map((prod, index) => (
+              <Card key={index} id={prod.id} nombre={prod.nombre} talla={prod?.tallaSeleccionada} precio={prod.precio_descuento ? prod.precio_descuento : prod.precio} cantidad={prod.cantidad} url={prod.url} alt={prod.alt} />
+            ))}
+          </div>
+          <p onClick={() => vaciarCarrito()}>Vaciar carrito....</p>
+        </>
       ) : (
         <div className={stylesCarrito.blankState}>
           <h3>Tú carrito está vacio</h3>
