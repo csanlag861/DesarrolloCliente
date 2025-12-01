3802cde057a5ccaa63f8fcc41d42fcf8dc4e725f
csanlag861 <csanlag861@g.educaand.es>
2025-12-01 13:05:49 +0100

    feat(Checkout): Calcular el total para miembros, y usuarios normales.

--- react/lovestar-ecommerce/src/components/Checkout/Productos/Productos.jsx
+++ react/lovestar-ecommerce/src/components/Checkout/Productos/Productos.jsx
@@ -4,17 +4,37 @@ import Card from "../../../components/Cards/Card-Checkout/Card";
 
 import { useContext } from "react";
 import { CartContext } from "../../../context/ContextCart";
+import { UserContext } from "../../../context/ContextUser";
 
 const Productos = () => {
-    const {carrito} = useContext(CartContext);
-    console.log(carrito);
+  const { carrito } = useContext(CartContext);
+  const { currentUser } = useContext(UserContext);
+
+  console.log(carrito);
+  console.log(currentUser);
   return (
     <section className={stylesProductos.section}>
-        {carrito.map((prod, index) => (
-            <Card key={index} url={prod.url} nombre={prod.nombre} precio={prod.precio} alt={prod.alt} talla={prod.tallaSeleccionada} cantidad={prod.cantidad} />
-        ))}
+      {carrito.map((prod, index) => (
+        <Card
+          key={index}
+          url={prod.url}
+          nombre={prod.nombre}
+          precio={prod.precio}
+          alt={prod.alt}
+          talla={prod.tallaSeleccionada}
+          cantidad={prod.cantidad}
+        />
+      ))}
+      {currentUser?.rol === "miembro" && <div className={stylesProductos.descuento}><p>Descuento de miembro: </p><p>-35%</p></div>}
+      <div className={stylesProductos.total}><p>Total:</p><p>€{calcularTotal(currentUser, carrito)}</p></div>
     </section>
   );
 };
 
 export default Productos;
+
+function calcularTotal(currentUser, carrito){
+  const totalCarrito = carrito.reduce((total, producto) => { return total + (producto.precio_descuento ? (Number(producto.precio_descuento)) : (Number(producto.precio))) * Number(producto.cantidad) }, 0)
+  const total = currentUser?.rol === "miembro" ? (totalCarrito * 0.65) : totalCarrito;
+  return total
+}
