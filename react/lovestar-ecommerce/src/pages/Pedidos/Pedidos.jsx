33c24dd25c6a07c4511b6be78ed21c9ab5896aa3
csanlag861 <csanlag861@g.educaand.es>
2025-12-02 21:32:27 +0100

    feat(Pedidos): Vista de Pedidos

--- react/lovestar-ecommerce/src/pages/Pedidos/Pedidos.jsx
+++ react/lovestar-ecommerce/src/pages/Pedidos/Pedidos.jsx
@@ -2,12 +2,62 @@ import stylesPedidos from "./pedidos.module.css";
 
 import Card from "../../components/Cards/Card-Pedidos/Card-Pedidos";
 
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "../../context/ContextUser";
+
+import { getPedidosByUser } from "../../utils/querys";
+
 function Pedidos() {
+  const { currentUser } = useContext(UserContext);
+
+  const [pedidosUsuario, setPedidosUsuario] = useState(null);
+
+  useEffect(() => {
+    if (currentUser) {
+
+      const getPedidos = async () => {
+        const pedidos = await getPedidosByUser(currentUser.uid)
+        setPedidosUsuario(pedidos);
+      }
+
+      getPedidos();
+    };
+  }, [currentUser])
+
+  console.log(pedidosUsuario);
+
+
+
   return (
     <section className={stylesPedidos.main}>
       <h1>Pedidos</h1>
       <div className={stylesPedidos.form}>
-        <Card />
+        {!pedidosUsuario ? (
+          <div className={stylesPedidos.noPedidos}>
+            <div className={stylesPedidos.info}>
+              <p className={stylesPedidos.titulo}>No hay pedidos aún.</p>
+              <p className={stylesPedidos.texto}>Explora nuestra tienda y encuentra algo especial.</p>
+            </div>
+          </div>
+        ) : (
+          <table>
+            <thead>
+              <tr>
+                <th></th>
+                <th>Pedido</th>
+                <th>Status</th>
+                <th>Total</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {pedidosUsuario.map((pedido, index) => (<tr>
+                <Card key={index} idPedido={pedido.id} total={pedido.total} fecha={pedido.fecha} estado={pedido.estado} url={pedido.items[0].url} items={pedido.items.length}/>
+              </tr>))}
+            </tbody>
+          </table>
+        )}
+
       </div>
     </section>
   );
