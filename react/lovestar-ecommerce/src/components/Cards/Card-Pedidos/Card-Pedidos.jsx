3c260f65c2b9b3292c7d3825ccb863660d99aab0
csanlag861 <csanlag861@g.educaand.es>
2025-12-02 21:32:03 +0100

    feat(card-pedido): Card para el pedido

--- react/lovestar-ecommerce/src/components/Cards/Card-Pedidos/Card-Pedidos.jsx
+++ react/lovestar-ecommerce/src/components/Cards/Card-Pedidos/Card-Pedidos.jsx
@@ -1,26 +1,31 @@
 import stylesCard from "./card-pedidos.module.css";
 
-import { UserContext } from "../../../context/ContextUser";
-import { useContext } from "react";
-
 import { Icon } from "@iconify/react";
 
-const Card = () => {
-  const { currentUser } = useContext(UserContext);
+const Card = ({ idPedido, estado, fecha, total, url, items }) => {
 
+  const fechaPedido = fecha?.toDate().toLocaleString("es-ES", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  });
   return (
-    <div className={stylesCard.pedidos}>
-      <div className={stylesCard.user}>
-        {currentUser?.pedidos ? (
-          "Pedidos hehe"
-        ) : (
-          <div className={stylesCard.info}>
-            <p className={stylesCard.titulo}>No hay pedidos aún.</p>
-            <p className={stylesCard.texto}>Explora nuestra tienda y encuentra algo especial.</p>
-          </div>
-        )}
-      </div>
-    </div>
+    <>
+      <td><img src={url} alt="" /></td>
+      <td>
+        <div className={stylesCard.text}>
+          <h3>{idPedido}</h3>
+          <p>Items: {items}</p>
+        </div>
+      </td>
+      <td>
+        <div className={stylesCard.text}>
+          <h3>{estado}</h3>
+          <p>{fechaPedido}</p>
+        </div>
+      </td>
+      <td>€{total}</td>
+    </>
   );
 };
 
