5e8a0e27bf90effb273ccc323262d63341514899
csanlag861 <csanlag861@g.educaand.es>
2025-12-05 13:08:33 +0100

    feat(CRUD-Usuario): Añadir el botón para editar rol con el html para las opciones.

--- react/lovestar-ecommerce/src/components/Cards/Card-Usuario/Card.jsx
+++ react/lovestar-ecommerce/src/components/Cards/Card-Usuario/Card.jsx
@@ -2,26 +2,48 @@ import stylesCard from "./card.module.css";
 
 import { Icon } from "@iconify/react";
 
-
 import { useState } from "react";
 
 const Card = ({ displayName, username, rol }) => {
   const [isDisabled, setIsDisabled] = useState(true);
+  const [editRol, setEditRol] = useState(false);
+  const [rolSeleccionado, setRolSeleccionado] = useState(null);
 
   return (
     <div className={stylesCard.card}>
       {displayName ? (
         <div className={stylesCard.nombre}>
-          <input name="username" type="text" value={displayName} disabled={isDisabled}></input>{" "}
-          <Icon icon="ri:edit-fill" onClick={() => setIsDisabled(!isDisabled)}/>
+          <input
+            name="username"
+            type="text"
+            value={displayName}
+            disabled={isDisabled}
+          ></input>{" "}
+          <Icon
+            icon="ri:edit-fill"
+            onClick={() => setIsDisabled(!isDisabled)}
+          />
         </div>
       ) : (
         <div className={stylesCard.nombre}>
           <input type="text" value={username} disabled={isDisabled}></input>
-          <Icon icon="ri:edit-fill" onClick={() => setIsDisabled(!isDisabled)}/>
+          <Icon
+            icon="ri:edit-fill"
+            onClick={() => setIsDisabled(!isDisabled)}
+          />
         </div>
       )}
-      <p>{rol}</p>
+      <div className={stylesCard.rol}>
+        {!editRol && <p>{rol}</p>}
+        {editRol && (
+          <select value={rol} onChange={(evento) => setRolSeleccionado(evento.target.value)}>
+            <option value="user">Usuario</option>
+            <option value="miembro">Miembro</option>
+            <option value="admin">Administrador</option>
+          </select>
+        )}
+        <Icon icon="ri:edit-fill" onClick={() => setEditRol(!editRol)} />
+      </div>
       <Icon icon="streamline-block:basic-ui-delete-user" />
     </div>
   );
