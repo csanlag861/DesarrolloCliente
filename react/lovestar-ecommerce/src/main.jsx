00bf3df4fc60e5c00a0b98f93962478ed1e344db
csanlag861 <csanlag861@g.educaand.es>
2025-12-05 17:28:14 +0100

    chore: Añadir React Query

--- react/lovestar-ecommerce/src/main.jsx
+++ react/lovestar-ecommerce/src/main.jsx
@@ -4,16 +4,20 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { UserContextProvider } from "./context/ContextUser";
 import { CartContextProvider } from "./context/ContextCart.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
 /*   <StrictMode>
  */    <UserContextProvider>
-      <CartContextProvider>
-        <BrowserRouter>
+    <CartContextProvider>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
           <App />
-        </BrowserRouter>
-      </CartContextProvider>
-    </UserContextProvider>
+        </QueryClientProvider>
+      </BrowserRouter>
+    </CartContextProvider>
+  </UserContextProvider>
 /*   </StrictMode>,
  */)
