8030c87c29039d39e9255382bad914dcc8ed26f0
csanlag861 <csanlag861@g.educaand.es>
2025-12-09 12:43:51 +0100

    feat(CRUD-Productos): Vista de productos en el CRUD de Admin.

---
+++ react/lovestar-ecommerce/src/components/Cards/Card-Producto/Card.jsx
@@ -0,0 +1,94 @@
+import stylesCard from "./card.module.css";
+
+import { Icon } from "@iconify/react";
+
+import { useState } from "react";
+
+import { useUpdateProduct, useDeleteProduct } from "../../../utils/react-query";
+
+import { toast } from "react-toastify";
+
+const Card = ({ producto }) => {
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [nombreProducto, setNombreProduct] = useState(producto.nombre);
+  const [precioProducto, setPrecioProducto] = useState(producto.precio);
+  const [categoriaProducto, setCategoriaProducto] = useState(
+    producto.categoria
+  );
+
+  const updateProduct = useUpdateProduct();
+
+  const deleteProduct = useDeleteProduct();
+
+  const handleSave = () => {
+    updateProduct.mutate(
+      { id: producto.id, data: { nombre: nombreProducto } },
+      {
+        onSuccess: () => {
+          toast.success("Producto actualizado correctamente");
+          setIsDisabled(true);
+        },
+        onError: () => {
+          toast.error("Error al actualizar el producto.");
+        },
+      }
+    );
+  };
+
+  const handleDelete = () => {
+    deleteProduct.mutate(
+      { id: producto.id },
+      {
+        onSuccess: () => {
+          toast.success("Producto eliminado correctamente.");
+        },
+        onError: (error) => {
+          console.error("Error al eliminar el producto", error);
+          toast.error("Error al eliminar el producto.");
+        },
+      }
+    );
+  };
+
+  return (
+    <div className={stylesCard.card}>
+      <img src={producto.url} alt={producto.alt} />
+      <input
+        type="text"
+        name="nombreProducto"
+        value={nombreProducto}
+        disabled={isDisabled}
+        onChange={(evento) => setNombreProduct(evento.target.value)}
+      />
+      {isDisabled && <p>{categoriaProducto}</p>}
+      {!isDisabled && (
+        <select
+          value={categoriaProducto}
+          onChange={(evento) => setCategoriaProducto(evento.target.value)}
+          disabled={isDisabled}
+        >
+          <option value="Gorros">Gorros</option>
+          <option value="Sudaderas">Sudaderas</option>
+          <option value="Jerseys">Jerseys</option>
+          <option value="Pantalones">Pantalones</option>
+          <option value="Camisetas">Camisetas</option>
+        </select>
+      )}
+
+      <input
+        type="text"
+        name="precio"
+        value={precioProducto}
+        disabled={isDisabled}
+        onChange={(evento) => setPrecioProducto(evento.target.value)}
+      />
+      <div className={stylesCard.opciones}>
+        <Icon icon="ri:edit-fill" onClick={() => setIsDisabled(!isDisabled)} />
+        <Icon icon="line-md:confirm-circle-filled" onClick={handleSave} />
+        <Icon icon="mdi:bin" onClick={handleDelete} />
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- react/lovestar-ecommerce/src/components/Dashboard/Producto/Producto.jsx
+++ react/lovestar-ecommerce/src/components/Dashboard/Producto/Producto.jsx
@@ -1,5 +1,35 @@
+import stylesProducto from "./producto.module.css";
+
+import {getAllProducts} from "../../../utils/querys";
+import { useEffect, useState } from "react";
+
+import Card from "../../Cards/Card-Producto/Card";
+
 const Producto = () => {
-    return (<div>Panel ADMIN producto</div>)
+    const [productos, setProductos] = useState([]);
+
+    useEffect(() => {
+        const fetchproductos = async () => {
+            const productos = await getAllProducts();
+            setProductos(productos);
+        }
+        
+        fetchproductos();
+    }, [productos])    
+    
+    console.log(productos);
+    
+    return (
+    <div className={stylesProducto.container}>
+        <div className={stylesProducto.titulo}>
+            <p>Imagen</p>
+            <p>Nombre del Producto</p>
+            <p>Categoria</p>
+            <p>Precio</p>
+            <p>Opciones</p>
+        </div>
+        {productos.map((prod, index) => (<Card key={index} producto={prod} />))}
+    </div>)
 }
 
 export default Producto;
